35a64161f69308887541b2bdef0002d549fc67e6
Robert <robert.nguyenanh@gmail.com>
2025-06-10 10:18:04 -0400

    Add MaxWidthWrapper and Heading components for layout and typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { MaxWidthWrapper } from "./components/max-width-wrapper";
+import React from "react";
+
 const Page = () => {
   return <>
     <section className="relative py-24 sm:py-32 bg-brand-25">
-      <MaxWidthWrapper></MaxWidthWrapper>
+      <MaxWidthWrapper className="text-center">
+        <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+          <div>
+            <h1></h1>
+          </div>
+        </div>
+      </MaxWidthWrapper>
     </section>
     <section></section>
     <section></section>
